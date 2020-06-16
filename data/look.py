32806a7d13e2564d54455974d58aee44353d70d1
18811736532 <394806861@qq.com>
2020-06-16 17:30:15 +0800

    data

---
+++ data/look.py
@@ -0,0 +1,8 @@
+import os
+import pandas as pd
+data_a = pd.read_csv('a/train.data/feature.tsv',sep='\t')    
+data_b = pd.read_csv('b/train.data/feature.tsv',sep='\t')  
+data_c = pd.read_csv('c/train.data/feature.tsv',sep='\t')  
+data_d = pd.read_csv('d/train.data/feature.tsv',sep='\t')  
+data_e = pd.read_csv('e/train.data/feature.tsv',sep='\t')  
+    
